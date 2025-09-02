4417b0db8edb6dc09f4f1883c5be5f6b98adc20b
runner <runner@pkrvmccyg1gnepe>
2025-09-02 07:33:53 +0000

    feat: v1.59.0

---
+++ Modules/LuxEditor/LuxEditor/Logic/ImageManager.cs
@@ -0,0 +1,63 @@
+﻿using LuxEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace LuxEditor.Services
+{
+    /// <summary>
+    /// Manages the list of editable images and handles selection state.
+    /// </summary>
+    public class ImageManager
+    {
+        private static readonly Lazy<ImageManager> _instance = new(() => new ImageManager());
+
+        /// <summary>
+        /// Gets the singleton instance of the ImageManager.
+        /// </summary>
+        public static ImageManager Instance => _instance.Value;
+
+        public ObservableCollection<EditableImage> OpenedImages { get; } = new();
+        public EditableImage? SelectedImage { get; private set; }
+
+        public event Action<EditableImage>? OnSelectionChanged;
+
+        private ImageManager() { }
+
+        /// <summary>
+        /// Loads a list of editable images into the manager.
+        /// </summary>
+        public void LoadImages(IEnumerable<EditableImage> images)
+        {
+            OpenedImages.Clear();
+
+            foreach (var img in images)
+            {
+                OpenedImages.Add(img);
+            }
+
+            if (OpenedImages.Count > 0)
+            {
+                SelectImage(OpenedImages[0]);
+            }
+        }
+
+        /// <summary>
+        /// Selects the given image and notifies subscribers.
+        /// </summary>
+        public void SelectImage(EditableImage image)
+        {
+            SelectedImage = image;
+            OnSelectionChanged?.Invoke(image);
+        }
+
+        /// <summary>
+        /// Clears all images and resets the selection.
+        /// </summary>
+        public void Clear()
+        {
+            OpenedImages.Clear();
+            SelectedImage = null;
+        }
+    }
+}
---
+++ Modules/LuxStudio/LuxStudio.COM/Models/LuxCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace LuxStudio.COM.Models;
+
+public class LuxCollection
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public ICollection<string> allowedEmails { get; set; } = new List<string>();
+    public ICollection<ChatMessage> ChatMessages { get; set; } = new List<ChatMessage>();
+    public ICollection<Photo> Photos { get; set; } = new List<Photo>();
+}
